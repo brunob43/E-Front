e89a48245d8367036394ef1b169c9dff77794a46
Bruno mac <brunob43@outlook.com>
2023-04-17 12:19:42 -0300

    3

--- src/app.tsx
+++ src/app.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>El Club de la Milanga</h1>
+      <h1>El Club de la Milanga(sin pablo)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
