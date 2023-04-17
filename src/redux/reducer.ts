2bb8c04e90d58aaf6511a78fd6d2dab232eecf0e
Bruno mac <brunob43@outlook.com>
2023-04-17 16:58:55 -0300

    store

---
+++ src/redux/reducer.ts
@@ -0,0 +1,12 @@
+import { Action } from "redux";
+
+
+const initialState = {};
+const rootReducer = (state: State = initialState, action: Action) => {
+    switch(action.type){
+   
+    }
+}
+
+export default rootReducer;
+
---
+++ src/redux/store.ts
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk';
+import  rootReducer  from './reducer'
+
+
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
